e2065f94d42ddc01b53f18e5ff3035008ea9595e
Arellano-Jann <jannrellano@nevada.unr.edu>
2023-03-24 13:15:56 -0700

    logs the current URL into the console

--- index.js
+++ index.js
@@ -3,18 +3,23 @@ const path = require('path');
 const members = require('./Members');
 
 const app = express();
+    
+const logger = (req, res, next) => { // logger middleware. next is a callback that moves on to the next middleware in the stack
+    console.log(`${req.protocol}://${req.get('host')}${req.originalUrl}`); // http :// localhost:5000 /api/members
+    next();
+}
+app.use(logger); // use the logger middleware
 
 // app.get('/', (req, res) => {
 //     console.log('Get Request');
 //     // res.send('<h1>Hello World</h1>');
 //     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 // })
-
-
-
+    
 app.get('/api/members', (req,res) => { // rest API - get
-     res.json(members); // sends the members array as a json object
+    res.json(members); // sends the members array as a json object
 });
+
 
 app.use(express.static(path.join(__dirname, 'public'))); // gets the current directory and joins it with the public folder to set the static path as the public folder
 
