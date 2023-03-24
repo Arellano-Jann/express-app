e265d812e91e00b0631397785143daff72c2c960
Arellano-Jann <jannrellano@nevada.unr.edu>
2023-03-24 12:47:45 -0700

    added static route

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
-const PORT = 5000 || process.env.PORT;
+// app.get('/', (req, res) => {
+//     console.log('Get Request');
+//     // res.send('<h1>Hello World</h1>');
+//     res.sendFile(path.join(__dirname, 'public', 'index.html'));
+// })
 
+
+app.use(express.static(path.join(__dirname, 'public'))); // gets the current directory and joins it with the public folder to set the static path as the public folder
+
+const PORT = 5000 || process.env.PORT;
 app.listen(PORT, () => {
     `Server started on port ${PORT}`
 });
