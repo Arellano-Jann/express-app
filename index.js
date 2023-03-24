19ffab21eaecb3f30c2ff432662e05a07025138c
Arellano-Jann <jannrellano@nevada.unr.edu>
2023-03-24 13:05:38 -0700

    added a get route

--- index.js
+++ index.js
@@ -9,6 +9,24 @@ const app = express();
 //     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 // })
 
+const members = [
+    {
+        id: 1,
+        name: 'John Doe',
+        email: 'asd@gmail.com',
+        status: 'active'
+    },
+    {
+        id: 2,
+        name: 'Bob Williams',
+        email: 'bob@gmail.com',
+        status: 'inactive'
+    },
+];
+
+app.get('/api/members', (req,res) => { // rest API - get
+     res.json(members); // sends the members array as a json object
+});
 
 app.use(express.static(path.join(__dirname, 'public'))); // gets the current directory and joins it with the public folder to set the static path as the public folder
 
