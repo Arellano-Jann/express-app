795f294c45a59ac877e1e5f3e7d6c1160311a705
Arellano-Jann <jannrellano@nevada.unr.edu>
2023-03-24 13:26:15 -0700

    added error checking

--- index.js
+++ index.js
@@ -19,7 +19,17 @@ app.get('/api/members', (req,res) => { // rest API - get
 
 app.get('/api/members/:id', (req, res) => {
     // res.send(req.params.id); // sends the id parameter
-    res.json(members.filter((member) => member.id === parseInt(req.params.id))); // parseInt turns the string into an int because member.id is a number. === means both types have to be the same
+
+    const found = members.some((member) => member.id === parseInt(req.params.id)); // some returns a boolean if the condition is true or false. the condition is that the member.id is equal to the id param.
+
+    if (found) {
+        res.json(members.filter((member) => member.id === parseInt(req.params.id))); // parseInt turns the string into an int because member.id is a number. === means both types have to be the same
+    }
+    else{
+        res.status(400).json({
+            msg: `Member with id ${req.params.id} not found`
+        });
+    }
 })
 
 
